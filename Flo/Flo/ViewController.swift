544c8099c567be98cd4104bd7856fea890829184
Aswin K V <aswin.kv@kuliza.com>
2018-06-03 23:51:36 +0530

    Added the first project -> Flo

---
+++ Flo/Flo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flo
+//
+//  Created by kuliza310 on 6/3/18.
+//  Copyright © 2018 kuliza310. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
